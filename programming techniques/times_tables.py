24a5f386b26b3e6344693b139be4b874412a602c
Jovan253 <54801590+Jovan253@users.noreply.github.com>
2019-09-18 13:09:53 +0100

    Update times_tables.py

--- programming techniques/times_tables.py
+++ programming techniques/times_tables.py
@@ -1,7 +1,18 @@
-table = int(input("What Table?"))
-rows = int(input("How many rows?"))
+rows = int(input("How many rows? "))
+
+answer == "n"
+while answer == "n":
+        table = -1
+        while table < 1 or table > 20:
+                table = input("What Table [1-20]")
+                if table.isdigit():
+                        table = int(table)
+                else:
+                        table = -1
+                        print("Not an integer")
+
+   answer = input("Did you mean Y/N: " , str(table)).lower())
 
 
-for item in range(1,rows):
-    print( (item, " times ", table) + "is " + (item * table))
-#next    
+
+  
