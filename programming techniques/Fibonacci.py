c379cfd86bcf830a37eeeb0169e535a908a7c5d3
Jovan253 <54801590+Jovan253@users.noreply.github.com>
2019-09-21 12:27:11 +0100

    Create Fibonacci.py

---
+++ programming techniques/Fibonacci.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+def fib1(n):
+    my_list[1,1]
+    for i in range(2,n):
+        my_list.append(my_list(i-2)+my_list[i-1])
+    #next
+    return my_list[n-1]    
+        
+#endfunction        
+        
+def fib2(n,my_list):
+    my_list.append(1)
+    my_list.append(1)
+    for i in range(2,n):
+        my_list.append(my_list[i-2]+my_list[i-1])
+    #next    
+#endprocedure
+
+print(my_list)
+
+    
