df45a06679a4ce697bd206b2946fe3008a9fe63d
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2019-09-22 16:25:13 +0100

    Feedback: Timestable
    The code is looking good, but doesn't seem to quite work. A few issues with identation. Make sure you follow the coding conventions. I also want to see validation and verification for both inputs.

--- programming techniques/times_tables.py
+++ programming techniques/times_tables.py
@@ -2,8 +2,12 @@ rows = int(input("How many rows? "))
 
 answer == "n"
 while answer == "n":
+
+### SRC - Too much indentation here (everything can go back 4 spaces
         table = -1
         while table < 1 or table > 20:
+ ### SRC - Too much indentation here (everything can go back 4 spaces
+               
                 table = input("What Table [1-20]")
                 if table.isdigit():
                         table = int(table)
@@ -11,7 +15,9 @@ while answer == "n":
                         table = -1
                         print("Not an integer")
 
-   answer = input("Did you mean Y/N: " , str(table)).lower())
+        answer = input("Did you mean Y/N: " , str(table).lower())
+
+   
 
 
 
